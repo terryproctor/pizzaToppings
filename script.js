67e8cce1d401fde4169b38f897a3da97458aa038
terryproctor <terryproctor2015@outlook.com>
2021-02-14 22:07:48 +0000

    new divs from js given h3's and classes

--- script.js
+++ script.js
@@ -8,19 +8,22 @@ function Topping(topping, stock, used) {
 	};
 }
 
-let peppers = new Topping('peppers', 30, 0);
-let anchovies = new Topping('anchovies', 10, 0);
-let pineapple = new Topping('pineapple', 15, 0);
+let peppers = new Topping('Peppers', 30, 0);
+let anchovies = new Topping('Anchovies', 10, 0);
+let pineapple = new Topping('Pineapple', 15, 0);
 
 let toppings = [peppers, anchovies, pineapple];
 
 for (let i=0; i<toppings.length; i++) {
 	let newEl = document.createElement('div');
-	let newText = document.createTextNode(toppings[i].topping);
-	newEl.appendChild(newText);
+	newEl.className = 'menuItem';
 	
-	let position = document.getElementById("menuContainer");
+	let newH3 = document.createElement('h3');
+	newEl.appendChild(newH3);
 
+	let position = document.getElementById("menuContainer");
+	let newText = document.createTextNode(toppings[i].topping);
+	newH3.appendChild(newText);
 	position.appendChild(newEl);
 
 
