c00b8003f8f5e44f8e86aaa7d3c90e90d85df2d3
terryproctor <terryproctor2015@outlook.com>
2021-02-15 21:31:13 +0000

    added additional class(background) + pointer

--- script.js
+++ script.js
@@ -3,29 +3,39 @@ function Topping(topping, stock, used) {
 	this.used = used;
   this.stock = stock;
 
-	this.percentage = function() {
+	this.percent = function() {
 		return ((this.stock - this.used) / stock) * 100	
 	};
 }
 
 let peppers = new Topping('Peppers', 30, 0);
-let anchovies = new Topping('Anchovies', 10, 0);
+let anchovies = new Topping('Anchovies', 10, 8);
 let pineapple = new Topping('Pineapple', 15, 0);
 
 let toppings = [peppers, anchovies, pineapple];
 
+let position = document.getElementById("menuContainer");
+
 for (let i=0; i<toppings.length; i++) {
 	let newEl = document.createElement('div');
 	newEl.className = 'menuItem';
+	newEl.style.cursor = 'pointer';
+	if (toppings[i].percent() > 25) {
+	newEl.className += ' greenBackground'
+	}
+	else if (toppings[i].percent() <= 25) {
+	newEl.className += ' redBackground'
+	} 
 	
 	let newH3 = document.createElement('h3');
 	newEl.appendChild(newH3);
 
-	let position = document.getElementById("menuContainer");
 	let newText = document.createTextNode(toppings[i].topping);
 	newH3.appendChild(newText);
 	position.appendChild(newEl);
-
-
+	
 }
-//need to add css classes to new elements
+
+toppings.forEach(Topping => {
+	console.log(Topping.percent())	
+});
